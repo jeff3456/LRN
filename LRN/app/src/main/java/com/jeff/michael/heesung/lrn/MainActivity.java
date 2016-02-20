83d3d124a76bd4c1ec70a0763fabc1f525b3293b
jeff3456 <jeff3456@gmail.com>
2016-02-19 21:02:06 -0500

    added Parse sdk

--- LRN/app/src/main/java/com/jeff/michael/heesung/lrn/MainActivity.java
+++ LRN/app/src/main/java/com/jeff/michael/heesung/lrn/MainActivity.java
@@ -5,9 +5,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.parse.ParseObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +20,13 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
---
+++ LRN/app/src/main/java/com/jeff/michael/heesung/lrn/ParseApplication.java
@@ -0,0 +1,25 @@
+package com.jeff.michael.heesung.lrn;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseUser;
+
+/**
+ * Created by jeff on 2/19/16.
+ */
+public class ParseApplication extends Application {
+
+    public ParseApplication() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this);
+        ParseUser.enableAutomaticUser();
+
+    }
+}
